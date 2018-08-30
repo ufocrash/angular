2794dae79cd00be46752adef50b3316bf9680c27
cristian.dinca <cristian.dinca@conversion.ro>
2018-08-30 16:30:28 +0300

    components update

--- src/app/succes-alert/succes.component.ts
+++ src/app/succes-alert/succes.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-succes',
-    template: `<div class="appSucces"><h4>This is the succes component!!!</h4></div>`,
+    template: `
+    <div class="appSucces">
+        <h4>Congratulations!!!</h4>
+        <p>You are the best!</p>
+    </div>`,
     styles: [`
         h4{
             color: green;
             text-align:center;
             font-size:30px;
             font-weight:bold;
+        }
+
+        p{
+            text-align:center;
         }
     `]
 })
